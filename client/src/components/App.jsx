9e96ad407a498710fdc1e91a804f76cbb2e6d4e2
Alvin Delito <alvindelito@live.com>
2020-09-29 13:42:36 -0700

    Fix dbHelper callback args, install Axios, Styled Components render

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import axios from 'axios';
+
+const Wrapper = styled.div`
+max-width: 1400px;
+background-color: #F8EBE6;
+margin: 0 auto;
+`;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      seller: [],
+      sellerProducts: [],
     };
+    this.getSellers = this.getSellers.bind(this);
+    this.getSellerProducts = this.getSellerProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getSellers();
+    this.getSellerProducts();
+  }
+
+  getSellers() {
+    axios.get('/api/seller/1')
+      .then((result) => this.setState({
+        seller: result.data,
+      }))
+      .catch((err) => console.error(err));
+  }
+
+  getSellerProducts() {
+    axios.get('/api/store/1')
+      .then((result) => this.setState({
+        sellerProducts: result.data,
+      }))
+      .catch((err) => console.error(err));
   }
 
   render() {
     return (
-      <div className="hi">
+      <Wrapper className="hi">
         <h1>hi</h1>
         <h2>hello</h2>
         <button className="test" type="submit">PUSH ME</button>
-      </div>
+      </Wrapper>
     );
   }
 }
--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -4,7 +4,7 @@ const getSeller = (id, callback) => {
   const queryStr = `SELECT * FROM sellers WHERE id=${id}`;
   db.query(queryStr, (err, result) => {
     if (err) callback(err);
-    else callback(result);
+    else callback(null, result);
   });
 };
 
@@ -12,7 +12,7 @@ const getSellerProducts = (id, callback) => {
   const queryStr = `SELECT * FROM stores, products WHERE stores.id=products.store_id AND stores.id=${id}`;
   db.query(queryStr, (err, result) => {
     if (err) callback(err);
-    else callback(result);
+    else callback(null, result);
   });
 };
 
