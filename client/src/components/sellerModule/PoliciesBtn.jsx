867ddca94408a496c1c63440253575b479c9cda2
Alvin Delito <alvindelito@live.com>
2020-10-05 16:34:41 -0700

    Policies button animation complete

--- client/src/components/sellerModule/PoliciesBtn.jsx
+++ client/src/components/sellerModule/PoliciesBtn.jsx
@@ -3,19 +3,37 @@ import styled from 'styled-components';
 import PolicyModal from './PolicyModal.jsx';
 
 const ShopPoliciesBtn = styled.button`
-  background-color: inherit;
+  background-color: #FFFFFF;
   border: none;
   font-weight: 400;
-  padding: 10px;
-  border-radius: 10px;
   margin-left: -10px;
   margin-bottom: 16px;
   outline: none;
+  cursor: pointer;
 
-  &:hover {
-    cursor: pointer;
-    background-color: #EEEEEE;
+  div {
+  position: absolute;
+  width: 130px;
+  height: 30px;
+  border-radius: 10px;
+  transform: scale(0);
+  transition: all 450ms ease 0ms;
   }
+
+  &:hover > div {
+    background: #EEEEEE;
+    transform: scale(1);
+    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+  }
+
+  span {
+    position: relative;
+    padding: 7px;
+    line-height: 30px;
+    left: 2px;
+  }
+
+
 `;
 
 export default class PoliciesBtn extends Component {
@@ -39,7 +57,10 @@ export default class PoliciesBtn extends Component {
     const { storeName } = this.props;
     return (
       <div>
-        <ShopPoliciesBtn type="button" onClick={this.toggleModal}>View shop policies</ShopPoliciesBtn>
+        <ShopPoliciesBtn type="button" onClick={this.toggleModal}>
+          <div />
+          <span>View shop policies</span>
+        </ShopPoliciesBtn>
         {
           showModal
             ? <PolicyModal storeName={storeName} toggleModal={this.toggleModal} />
